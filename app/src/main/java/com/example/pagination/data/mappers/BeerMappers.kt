0f43800e9384084a97a4a31c8d705af35ea38fc2
okezino <okezi002@gmail.com>
2023-07-08 21:23:45 +0400

    create the remote mediator

---
+++ app/src/main/java/com/example/pagination/data/mappers/BeerMappers.kt
@@ -0,0 +1,27 @@
+package com.example.pagination.data.mappers
+
+import com.example.pagination.data.local.BeerEntity
+import com.example.pagination.data.remote.BeerDto
+import com.example.pagination.domain.Beer
+
+fun BeerDto.toBeerEntity(): BeerEntity {
+    return BeerEntity(
+        id = id,
+        name = name,
+        tagline = tagline,
+        description = description,
+        firstBrewed = first_brewed,
+        imageUrl = image_url
+    )
+}
+
+fun BeerEntity.toBeer(): Beer {
+    return Beer(
+        id = id,
+        name = name,
+        tagline = tagline,
+        description = description,
+        firstBrewed = firstBrewed,
+        imageUrl = imageUrl
+    )
+}
